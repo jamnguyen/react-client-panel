7afae41a0bc4a73b810cce17d727e21e18d1551a
jamnguyen <jam@jamnguyen.com>
2018-11-11 22:17:18 +0700

    Get clients from DB.

--- src/clients/Clients.js
+++ src/clients/Clients.js
@@ -1,87 +1,94 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import Spinner from '../layout/Spinner';
 
 class Clients extends Component {
   state = {
-    clients: [
-      {
-        id: '1',
-        firstName: 'Jam',
-        lastName: 'Nguyen',
-        email: 'jam@jamnguyen.com',
-        phone: '111-111-0000',
-        balance: '756.4356'
-      },
-      {
-        id: '2',
-        firstName: 'Steve',
-        lastName: 'Roger',
-        email: 'steve@jamnguyen.com',
-        phone: '111-111-0001',
-        balance: '1200.36'
-      },
-      {
-        id: '3',
-        firstName: 'Tony',
-        lastName: 'Stark',
-        email: 'stark@jamnguyen.com',
-        phone: '111-111-0002',
-        balance: '16400'
-      },
-    ]
+    totalOwed: null
+  }
+
+  // This's invoked right before render(), when clients come
+  static getDerivedStateFromProps(props, state) {
+    const { clients } = props;
+    if (clients) {
+      const total = clients.reduce((total, client) => {
+        return total += parseFloat(client.balance.toString());
+      }, 0);
+      return { totalOwed: total };
+    }
+
+    return null;
   }
 
   render() {
-    if (this.state.clients) {
-      const clientRows = this.state.clients.map(client => {
-        return (
-          <tr key={ client.id }>
-            <td>{ client.firstName } { client.lastName }</td>
-            <td>{ client.email }</td>
-            <td>{ parseFloat(client.balance).toFixed(2) }</td>
-            <td>
-              <Link className="btn btn-secondary btn-sm" to={ `/client/${client.id}` }>
-                <i className="fas fa-arrow-circle-right"></i> Details
-              </Link>
-            </td>
-          </tr>
-        );
-      });
+    const { clients } = this.props;
+    const { totalOwed } = this.state;
 
+    const clientRows = clients ? clients.map(client => {
       return (
-        <React.Fragment>
-          {/* Title */}
-          <div className="row mb-4">
-            <div className="col-md-6">
-              <h1><i className="fas fa-users"></i> Clients</h1>
-            </div>
-            <div className="col-md-6">
-              {/* Total balance */}
-            </div>
+        <tr key={ client.id }>
+          <td>{ client.firstName } { client.lastName }</td>
+          <td>{ client.email }</td>
+          <td>{ parseFloat(client.balance).toFixed(2) }</td>
+          <td>
+            <Link className="btn btn-secondary btn-sm" to={ `/client/${client.id}` }>
+              <i className="fas fa-arrow-circle-right"></i> Details
+            </Link>
+          </td>
+        </tr>
+      );
+    }) : null;
+
+    return (
+      <React.Fragment>
+        {/* Title */}
+        <div className="row mb-4">
+          <div className="col-md-6">
+            <h1><i className="fas fa-users"></i> Clients</h1>
           </div>
-          {/* Clients table */}
+          <div className="col-md-6 d-flex align-items-center justify-content-end">
+            { clients ? (
+              <h4>
+                Total Owed: <span className="text-primary">${ totalOwed.toFixed(2) }</span>
+              </h4>
+            ) : null}
+          </div>
+        </div>
+        {/* Clients table */}
+        { clients ? (
           <table className="table table-striped">
-            <thead className="thead-inverse">
-              <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Balance</th>
-              <th />
-              </tr>
-            </thead>
-            <tbody>
-              { clientRows }
-            </tbody>
-          </table>
-  
-        </React.Fragment>
-      );
-    } else {
-      return (
-        <div>Loading...</div>
-      );
-    }
+          <thead className="thead-inverse">
+            <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Balance</th>
+            <th />
+            </tr>
+          </thead>
+          <tbody>
+            { clientRows }
+          </tbody>
+        </table>
+        ) : (
+          <Spinner />
+        ) }
+      </React.Fragment>
+    );
   }
 }
 
-export default Clients;
+Clients.propTypes = {
+  firestore: PropTypes.object.isRequired,
+  clients: PropTypes.array
+}
+
+export default compose(
+  firestoreConnect([{ collection: 'clients' }]),
+  connect((state, props) => ({
+    clients: state.firestore.ordered.clients
+  }))
+)(Clients);
---
+++ src/layout/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div className="d-flex justify-content-center">
+      <i className="text-primary fas fa-cog fa-spin" style={{ fontSize: '3rem' }}></i>
+    </div>
+  )
+}
+
+export default Spinner;
